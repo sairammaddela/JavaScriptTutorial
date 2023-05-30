f9daebb5bbbedb17090138d20aee3e255cabe7a4
sairammaddela <sairammaddela2@gmail.com>
2023-05-30 22:31:46 +0530

    IIFE

--- First.js
+++ First.js
@@ -3,4 +3,12 @@ var lastname="Maddela";
 console.log(firstname,lastname);
 var a="sairam";
 a=1224;
-console.log(a);
+console.log(a,typeof a);
+(function callOnce(){
+    console.log("Calling only once 1");
+})();
+(
+    ()=>{
+        console.log("Calling only once 2");
+    }
+)();
